2d5a84f466950001c955864ebc31150d9eb4fb3f
Mike <retiredtotuscany@gmail.com>
2024-12-05 05:54:48 -0500

    renamed educatedowlbooks to marketplace

--- eob/eob/urls.py
+++ eob/eob/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from educatedowlbooks import views
+from marketplace import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
