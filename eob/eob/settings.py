446b3fd7876d0940872c62b612cf923393e99546
Mike <retiredtotuscany@gmail.com>
2024-12-06 04:51:26 -0500

    dddd

--- eob/eob/settings.py
+++ eob/eob/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'educatedowlbooks/static/'
+STATIC_URL = 'marketplace/static/'
 
 
 # Default primary key field type
