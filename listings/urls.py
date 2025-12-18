0fa73ce2c7184be961e23621964ee8de3f6e13a4
Mike <retiredtotuscany@gmail.com>
2025-12-18 08:21:22 -0500

    added urls.py to all sections

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import public_listings
+from . import views
+
 
 urlpatterns = [
-    path("", public_listings, name="public_listings"),
+    path("", views.public_listings, name="public_listings"),
 ]
