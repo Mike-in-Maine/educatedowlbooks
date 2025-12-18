9d688353db46ad399b3f5ae1aada4404cae289f4
Mike <retiredtotuscany@gmail.com>
2025-12-18 17:39:18 -0500

    secret key fixed

--- config/settings_base.py
+++ config/settings_base.py
@@ -3,8 +3,6 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # Application definition
 INSTALLED_APPS = [
--- config/settings_dev.py
+++ config/settings_dev.py
@@ -1,5 +1,5 @@
 from .settings_base import *
 
 DEBUG = True
-
+SECRET_KEY = "m4@jabq^2q$ch!k4mdn%j20)tncs68dbf55!!dp3oe#9ha=%bn"
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
