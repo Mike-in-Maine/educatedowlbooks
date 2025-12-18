be12577f0e91a013ec66cb8e76f8bb98879b484d
Mike <retiredtotuscany@gmail.com>
2025-12-18 08:26:48 -0500

    settings_prod

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,11 +1,29 @@
 from .settings_base import *
-
+import os
 DEBUG = False
 
 ALLOWED_HOSTS = [
     "educatedowlbooks.com",
     "www.educatedowlbooks.com",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://educatedowlbooks.com",
+    "https://www.educatedowlbooks.com",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "educatedowlbooks",
+        "USER": "eob_user",
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 SECURE_SSL_REDIRECT = True
 
