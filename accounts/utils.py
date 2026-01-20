df12981f78c752c183224d590a056aa1d8cd1ab4
Mike <retiredtotuscany@gmail.com>
2026-01-20 11:25:13 -0500

    gdfgdfg

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,8 +1,15 @@
+from functools import wraps
 from django.shortcuts import redirect
 
 def seller_required(view_func):
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect("login")
+
         if not hasattr(request.user, "seller_profile"):
             return redirect("seller_onboarding")
+
         return view_func(request, *args, **kwargs)
+
     return wrapper
